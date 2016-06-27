a74a395a5da28108094aaa50b17233c1301d6b09
aakashkh <aakashkh284@gmail.com>
2016-06-28 02:36:16 +0530

    fitted random walk to MSFT and S&P500 - 28/6/16

--- White Noise and Random Walks in Time Series Analysis.R
+++ White Noise and Random Walks in Time Series Analysis.R
@@ -13,3 +13,25 @@ plot(x,type = "l")
 acf(x)
 
 # Fitting random walks to financial data
+# Check that simulated random walk is good
+" definition of a random walk, which is simply that the difference between two 
+neighbouring values is equal to a realisation from a discrete white noise process.
+Hence, if we create a series of the differences of elements from our simulated series, 
+we should have a series that resembles discrete white noise "
+acf(diff(x)) # output shows that it is a white noise and hence good fit
+
+#Random walk model on Microsfot (MSFT) data
+library(quantmod)
+getSymbols('MSFT', src = 'yahoo')
+MSFT # microsoft data
+# Op(MSFT) , Hi(MSFT) , Lo(MSFT) , Cl(MSFT) , Vo(MSFT) , Ad(MSFT) .
+# The above commands can be used to obtain the Open, High, Low, Close, Volume and Adjusted Close prices for the Microsoft stock
+
+acf(diff(Ad(MSFT)), na.action = na.omit)
+#na.action = na.omit ignores missing values by omitting them
+#Output suggest that adjusting close price of Microsoft is a random walk
+
+# For S&P Index
+getSymbols('^GSPC', src='yahoo')
+acf(diff(Ad(GSPC)), na.action = na.omit)
+#Output suggest that there are many peaks at different levels and hence random walk is not a good fit.
