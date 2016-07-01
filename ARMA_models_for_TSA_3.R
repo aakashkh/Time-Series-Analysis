3c1bda091bff1f921784425b5f8290e1b0d1d422
aakashkh <aakashkh284@gmail.com>
2016-07-01 20:10:30 +0530

    Try to find best ARMA model on S&P 500 equity index, ARMA(3,3) but not a good model according to Ljung-Box Test - 1/7/16

--- ARMA_models_for_TSA_3.R
+++ ARMA_models_for_TSA_3.R
@@ -28,6 +28,7 @@ arima(x2, order=c(2, 0, 2))
 for a series, we needto use the AIC (or BIC) across a subset of values 
 for , and then apply the Ljung-Box test todetermine if a good fit has 
 been achieved, for particular values of"
+
 # ARMA (3,2), best ARMA(p,q) model
 set.seed(3)
 x3 <- arima.sim(n=1000, model=list(ar=c(0.5, -0.25, 0.4), ma=c(0.5, -0.3)))
@@ -50,3 +51,27 @@ acf(resid(final.arma)) # actually a DWN
 # Ljung-Box test for 20 lags to confirm this
 Box.test(resid(final.arma), lag=20, type="Ljung‐Box")
 # p-value = 0.869 > 0.05 and hence model is good fit
+
+# ARMA(p,q) on financial data -  S&P500 US Equity Index
+library(quantmod)
+getSymbols("^GSPC")
+sp = diff(log(Cl(GSPC)))
+# check which model fit best
+spfinal.aic <- Inf
+spfinal.order <- c(0,0,0)
+for (i in 0:4) 
+  for (j in 0:4) {
+    spcurrent.aic <- AIC(arima(sp, order=c(i, 0, j)))
+    if (spcurrent.aic < spfinal.aic) {
+      spfinal.aic <- spcurrent.aic
+      spfinal.order <- c(i, 0, j)
+      spfinal.arma <- arima(sp, order=spfinal.order)
+    }
+  }
+
+spfinal.order
+acf(resid(spfinal.arma), na.action=na.omit) # residuals of the fitted model
+# Sort of poor fit, but better than previous
+# Ljung-Box test to see whether ARMA(3,3) a good fit or not
+Box.test(resid(spfinal.arma), lag=20, type="Ljung‐Box")
+# p-value = 0.01034 is less than .05 and hence not a good model
