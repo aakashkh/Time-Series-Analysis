cee2659b67e3d75728ab93d652b31149b583c3aa
aakashkh <aakashkh284@gmail.com>
2016-07-02 19:49:02 +0530

    Fitted ARIMA on Amazon and S&P500 and forecasted 25 days ahead - 2/7/16

--- ARIMA_models_for_TSA.R
+++ ARIMA_models_for_TSA.R
@@ -20,3 +20,60 @@ acf(resid(x.arima))
 # Box-Ljung test to check the model
 Box.test(resid(x.arima), lag=20, type = "Ljung-Box")
 # p-value = .5191 is > 0.05 and therefore DWN is a godd fit to residuals
+# Financial Data and Prediction
+library(quantmod)
+library(forecast)
+#Amazon
+getSymbols("AMZN", from="2013-01-01")
+amzn = diff(log(Cl(AMZN))) # already taken first order difference
+# Find the best ARIMA model
+azfinal.aic <- Inf
+azfinal.order <- c(0,0,0)
+for (p in 1:4) 
+  for (d in 0:1) 
+    for (q in 1:4) {
+      azcurrent.aic <- AIC(arima(amzn, order=c(p, d, q)))
+      if (azcurrent.aic < azfinal.aic) {
+        azfinal.aic <- azcurrent.aic    
+        azfinal.order <- c(p, d, q)    
+        azfinal.arima <- arima(amzn, order=azfinal.order)  
+     }
+    }
+azfinal.order # ARIMA(4,0,4)
+acf(resid(azfinal.arima), na.action=na.omit) # residual as DWN or not
+# Perfect fit visible
+# Ljung-Box test for p-value
+Box.test(resid(azfinal.arima), lag=20, type="Ljung-Box")
+# p-value = .9911 > 0.05 and hence we have evidence for good fit at 95% level
+# Forecast using forecast library
+plot(forecast(azfinal.arima, h=25))
+# 25 days ahead, 95% dark blue, 99% light blue
+
+# S&P 500
+getSymbols("^GSPC", from="2013-01-01")
+sp = diff(log(Cl(GSPC))) # already taken first order difference
+# Find the best ARIMA model
+spfinal.aic <- Inf
+spfinal.order <- c(0,0,0)
+for (p in 1:4) 
+  for (d in 0:1) 
+    for (q in 1:4) {
+      spcurrent.aic <- AIC(arima(sp, order=c(p, d, q)))
+      if (spcurrent.aic < spfinal.aic) {
+        spfinal.aic <- spcurrent.aic
+        spfinal.order <- c(p, d, q)
+        spfinal.arima <- arima(sp, order=spfinal.order) 
+      }
+    }
+spfinal.order # ARIMA(1,0,2)
+acf(resid(spfinal.arima), na.action=na.omit) # residual as DWN or not
+# Perfect fit visible but long term memory effects
+# Ljung-Box test for p-value
+Box.test(resid(spfinal.arima), lag=20, type="Ljung-Box")
+# p-value = .7148 > 0.05 and hence we have evidence for good fit at 95% level
+# Forecast using forecast library
+plot(forecast(spfinal.arima, h=25))
+# 25 days ahead, 95% dark blue, 99% light blue
+# In quantitative finance, trying to determine periods of differing volatility 
+# is often known as "regime detection". It is one of the harder tasks to achiev.
+# Hence 2007-08, volatile period is been excluded
